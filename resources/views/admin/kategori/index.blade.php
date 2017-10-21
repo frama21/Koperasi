2f2e4e3100fa12636af43936346e221298284c67
Fajar Ramdani A <greatofjar@gmail.com>
2017-10-21 21:33:49 +0700

    Update Index Kategori

--- resources/views/admin/kategori/index.blade.php
+++ resources/views/admin/kategori/index.blade.php
@@ -5,53 +5,30 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="/admin/kategoripinjaman">
-
-                        <div class="form-group{{ $errors->has('pinjaman_id') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Nama Kategori</label>
-
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="pinjaman_id" required autofocus>
-
-                                @if ($errors->has('pinjaman_id'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('pinjaman_id') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Submit
-                                </button>
-                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            </div>
-                        </div>
-                    </form>
+                {!! Form::open(['method'=>'GET','url'=>'/admin/kategoripinjaman/carikategori','role'=>'search']) !!}
+                    <div class="input-group custom-search-from">
+                        <input type="text" class="form-control" name="search" placeholder="Search...">
+                        <span class="input-group-btn">
+                            <span class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="fa fa-search"></i> Cari</button>
+                        </span>
+                    </span>
                 </div>
-
-
-
+                    {!! Form::close() !!}
             </div>
+
             <div class="panel panel-default">
                 <table class="table">
                     <thead>
                         <tr>
                             <th>Nama Kategori</th>
-                            <th>Pinjam Pada Tgl</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                        @foreach($Kategori as $datakategori)
                         <tr>
-                            <td>{{ $datakategori -> pinjaman_id }}</td>
-                            <td>{{ $datakategori -> created_at }}</td>
+                            <td>{{ $datakategori -> nama_pinjaman }}</td>
                              <td>
                                 <a type="button" class="btn btn-primary" href="/admin/kategoripinjaman/{{$datakategori->id}}/edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
                             </td>
@@ -70,8 +47,39 @@
                     </tbody>
                 </table>
             </div>
-        </div>
-                        
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="/admin/kategoripinjaman">
+
+                        <div class="form-group{{ $errors->has('nama_pinjaman') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Nama Kategori</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="nama_pinjaman">
+
+                                @if ($errors->has('nama_pinjaman'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('nama_pinjaman') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>      
     </div>
 </div>
 @endsection
